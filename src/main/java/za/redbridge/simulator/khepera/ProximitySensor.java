abfab4c87468ff90c066b80162e9dca2b0e0bee5
Jamie Hewland <jhewland@gmail.com>
2014-09-25 16:39:51 +0200

    Add Khepera III phenotype and sensors

---
+++ src/main/java/za/redbridge/simulator/khepera/ProximitySensor.java
@@ -0,0 +1,66 @@
+package za.redbridge.simulator.khepera;
+
+import org.apache.commons.math3.distribution.GammaDistribution;
+
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+
+import za.redbridge.simulator.sensor.AgentSensor;
+import za.redbridge.simulator.sensor.sensedobjects.SensedObject;
+
+/**
+ * A rough estimation of the ultrasonic sensor used in the Khepera III robot: the Vishay TCRT5000
+ * Created by jamie on 2014/09/23.
+ */
+public class ProximitySensor extends AgentSensor {
+
+    private static final float PROXIMITY_SENSOR_RANGE = 0.2f;
+    private static final float PROXIMITY_SENSOR_FOV = 0.2f; // This is a guess
+
+    private final GammaDistribution function = new GammaDistribution(2.5, 2.0);
+
+    public ProximitySensor(float bearing, float orientation) {
+        this(bearing, orientation, PROXIMITY_SENSOR_RANGE, PROXIMITY_SENSOR_FOV);
+    }
+
+    public ProximitySensor(float bearing, float orientation, float range, float fieldOfView) {
+        super(bearing, orientation, range, fieldOfView);
+    }
+
+    @Override
+    protected void provideObjectReading(List<SensedObject> sensedObjects, List<Double> output) {
+        if (!sensedObjects.isEmpty()) {
+            output.add(readingCurve(sensedObjects.get(0).getDistance()));
+        } else {
+            output.add(0.0);
+        }
+    }
+
+    @Override
+    public AgentSensor clone() {
+        return new ProximitySensor(bearing, orientation, range, fieldOfView);
+    }
+
+    @Override
+    public int getReadingSize() {
+        return 1;
+    }
+
+    @Override
+    public void readAdditionalConfigs(Map<String, Object> stringObjectMap) throws ParseException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Map<String, Object> getAdditionalConfigs() {
+        return null;
+    }
+
+    private double readingCurve(float distance) {
+        // Output curve of the TCRT5000 seems to produce something like a Gamma distribution curve
+        // See the datasheet for more information
+        return function.density(distance * 1000) * 6.64;
+    }
+
+}
