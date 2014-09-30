90573f30496140ab1dc88d9cac2b01bf67e48b98
Alexander Borysov <borysovalexander@gmail.com>
2014-09-30 16:43:23 +0200

    Added commenting to SensorVariable; added some whitespace as well.

--- src/main/java/za/redbridge/simulator/gp/SensorVariable.java
+++ src/main/java/za/redbridge/simulator/gp/SensorVariable.java
@@ -2,6 +2,7 @@ package za.redbridge.simulator.gp;
 
 import org.epochx.epox.Variable;
 import za.redbridge.simulator.gp.types.Bearing;
+import za.redbridge.simulator.gp.types.FloatVariable;
 import za.redbridge.simulator.gp.types.ProximityReading;
 import za.redbridge.simulator.gp.types.RelativeCoordinate;
 import za.redbridge.simulator.sensor.AgentSensor;
@@ -9,6 +10,8 @@ import za.redbridge.simulator.sensor.Sensor;
 
 /**
  * Created by xenos on 9/29/14.
+ * A class to tie the sensor parameters to the input variable; it will then produce ProximityReading objects with the same parameters set.
+ * This allows us to convert ProximityReading objects to their RelativeCoordinate, Bearing and FloatVariable (distance) counterparts.
  */
 public class SensorVariable extends Variable {
     private final float bearing;
@@ -35,6 +38,6 @@ public class SensorVariable extends Variable {
         if (value != null && !innerDatatype.isAssignableFrom(value.getClass())) {
             throw new IllegalArgumentException("Variables may not change data-type");
         }
-        super.setValue(new ProximityReading(((Number)value).floatValue(), bearing, range, fov));
+        super.setValue(new ProximityReading(((Number) value).floatValue(), bearing, range, fov));
     }
 }
