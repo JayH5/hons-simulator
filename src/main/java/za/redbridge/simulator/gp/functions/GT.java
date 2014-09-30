c34835c82a0cebe9c23ec58da5ee052d03fe2ef4
Alexander Borysov <borysovalexander@gmail.com>
2014-09-30 18:44:52 +0200

    Added debugging aid for GT having literal children

--- src/main/java/za/redbridge/simulator/gp/functions/GT.java
+++ src/main/java/za/redbridge/simulator/gp/functions/GT.java
@@ -15,6 +15,9 @@ public class GT extends Node {
 
     public GT(final Node c1, final Node c2){
         super(c1,c2);
+        if(c1 != null && c2 != null && !isLegit(c1.getClass(), c2.getClass())){
+            throw new RuntimeException("Tried to manually construct invalid GT");
+        }
     }
     @Override
     public String getIdentifier(){
@@ -25,8 +28,19 @@ public class GT extends Node {
         Object x = getChild(0).evaluate();
         Object y = getChild(1).evaluate();
         if(isLegit(x.getClass(), y.getClass())) return ((GPFloat) x).getValue() > ((GPFloat) y).getValue();
-        else return null;
+        else throw new RuntimeException("GT fail: " + x.getClass() + ", " + y.getClass());
     }
+    @Override
+     public void setChild(int i, Node c){
+         super.setChild(i,c);
+         if(c.getReturnType() == GPFloatLiteral.class){
+             int otherI = (i == 1 ? 0 : 1);
+             Object otherChild = getChild(otherI);
+             if(otherChild != null && getChild(otherI).getReturnType() == GPFloatLiteral.class){
+                 throw new RuntimeException("Attempted to assign a second GPFloatLiteral to GT");
+             }
+         }
+     }
 
     @Override
     public Class<?> getReturnType(final Class<?> ... inputTypes) {
