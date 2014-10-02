de34ab67be4e7f7d5622010e607ae0dcaf4e2344
Alexander Borysov <borysovalexander@gmail.com>
2014-10-02 15:28:27 +0200

    Changed tournament size to 10

--- src/main/java/za/redbridge/simulator/experiment/Main.java
+++ src/main/java/za/redbridge/simulator/experiment/Main.java
@@ -100,7 +100,7 @@ public class Main {
         model.setMaxDepth(7);
         model.setPopulationSize(200);
         model.setPoolSize(model.getPopulationSize() / 2);
-        model.setProgramSelector(new TournamentSelector(model, 4));
+        model.setProgramSelector(new TournamentSelector(model, 10));
         model.setNoRuns(1);
         model.setNoElites(model.getPopulationSize() / 10);
         model.setInitialiser(new RampedHalfAndHalfInitialiser(model));
