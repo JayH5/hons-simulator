cdc6f0126842778becf9def423bc6b69fb0ea1f7
mracter <paracetamolboy@gmail.com>
2014-10-08 14:18:36 +0200

    WIP thresholding and sensitivity

---
+++ src/main/java/za/redbridge/simulator/sensor/LinearObjectProximityAgentSensor.java
@@ -0,0 +1,13 @@
+package za.redbridge.simulator.sensor;
+
+/**
+ * Created by shsu on 2014/10/08.
+ */
+public class LinearObjectProximityAgentSensor {
+
+    public LinearObjectProximityAgentSensor() {
+
+    }
+
+
+}
