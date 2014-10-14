a3623935da6a075addce8aeaa22cdea011133d1c
Jamie Hewland <jhewland@gmail.com>
2014-10-14 16:05:32 +0200

    Increase blame box expansion rate

--- src/main/java/za/redbridge/simulator/object/TargetAreaObject.java
+++ src/main/java/za/redbridge/simulator/object/TargetAreaObject.java
@@ -36,7 +36,7 @@ public class TargetAreaObject extends PhysicalObject implements Collideable {
 
     private static final boolean ALLOW_REMOVAL = true;
 
-    private static final float BLAME_BOX_EXPANSION_RATE = 1.05f;
+    private static final float BLAME_BOX_EXPANSION_RATE = 1.2f;
     private static final int BLAME_BOX_TRIES = 5;
 
     private int width, height;
