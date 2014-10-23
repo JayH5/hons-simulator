62658cb1e06bbd2901aad8be0502647596915d22
mracter <paracetamolboy@gmail.com>
2014-10-23 15:54:00 +0200

    final params

---
+++ src/main/java/za/redbridge/simulator/stats/StatsProcessor.java
@@ -0,0 +1,12 @@
+package za.redbridge.simulator.stats;
+
+/**
+ * Created by shsu on 2014/10/23.
+ * Given a directory full of all the log files for one team setup, divides the files into the appropriate task difficulties
+ * and calculates stats based on those.
+ */
+public class StatsProcessor {
+
+
+
+}
